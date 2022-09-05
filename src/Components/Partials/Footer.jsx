c70179397f30c109783ffdd2520d922615a94b45
Gawmane <gawman.e@hotmail.com>
2022-09-05 10:33:33 +0200

    Footer

--- src/Components/Partials/Footer.jsx
+++ src/Components/Partials/Footer.jsx
@@ -1,9 +1,21 @@
 import style from '../../assets/Style/Footer.module.scss'
-
+import { AiFillTwitterSquare, AiFillFacebook } from "react-icons/ai";
 export const Footer = () => {
     return (
         <footer>
-            <p></p>
+            <div className={style.wrapper}>
+                <h2>HomeLands</h2>
+                <span>
+                    <p>Øster Uttrupvej 5 </p><p>9000 Aalborg</p>
+                </span>
+                <span>
+                    <a href="mailto:info@homelands.dk">Email: info@homelands.dk</a>
+                    <a href="tel:+45 1122 3344">Telefon: +45 1122 3344</a>
+                </span>
+            </div>
+            <span className={style.icons}>
+                <AiFillTwitterSquare /> <AiFillFacebook />
+            </span>
         </footer>
     )
 }
